f9ffb9b68f5870f5bdd653eabcdc0738bd6d845e
dapraws <darrelprawira26@gmail.com>
2024-09-22 22:52:58 +0700

    feat: dashboard view + controller

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -1,23 +1,58 @@
 import 'package:get/get.dart';
 
 class DashboardController extends GetxController {
-  //TODO: Implement DashboardController
+  final List<Map<String, String>> categories = [
+    {
+      "name": "Mahir Teknologi",
+      "image": "assets/images/img_mahirteknologi.png"
+    },
+    {"name": "Mahir Digital", "image": "assets/images/img_mahirdigital.png"},
+    {
+      "name": "Mahir Marketing",
+      "image": "assets/images/img_mahirmarketing.png"
+    },
+    {"name": "Mahir Bahasa", "image": "assets/images/img_mahirbahasa.png"},
+    {"name": "Mahir\nBisnis", "image": "assets/images/img_mahirbisnis.png"},
+    {"name": "Mahir Desain", "image": "assets/images/img_mahirdesain.png"},
+    {"name": "Mahir Masak", "image": "assets/images/img_mahirmasak.png"},
+    {"name": "Mahir Admin", "image": "assets/images/img_mahiradmin.png"},
+  ];
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
+  final List<String> partners = [
+    "assets/images/img_skillacademy.png",
+    "assets/images/img_arkademi.png",
+    "assets/images/img_cakap.png",
+    "assets/images/img_binuscenter.png",
+    "assets/images/img_myedusolve.png",
+    "assets/images/img_sertifikasiku.png",
+    "assets/images/img_vokraf.png",
+    "assets/images/img_alterra.png",
+  ];
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
+  List<Map<String, String>> reviews = [
+    {
+      "photo": "https://picsum.photos/200",
+      "name": "Gloria Arnasari",
+      "jobTitle": "Head of Designer at PT. XYZ",
+      "title": "Pengalaman belajar mendorong saya ke level berikutnya",
+      "review":
+          "Saya selalu mencari cara untuk meningkatkan keterampilan tim saya dan mengikuti perkembangan teknologi terbaru. Aplikasi online class ini benar-benar membantu kami untuk mencapai tujuan tersebut. Dengan akses ke berbagai kursus berkualitas tinggi, kami dapat belajar kapan saja dan di mana saja. Materi yang disediakan sangat relevan dan mudah dipahami, terutama dalam bidang desain dan teknologi.",
+    },
+    {
+      "photo": "https://picsum.photos/300",
+      "name": "Gloria Arnasari",
+      "jobTitle": "Head of Designer at PT. XYZ",
+      "title": "Pengalaman belajar mendorong saya ke level berikutnya",
+      "review":
+          "Saya selalu mencari cara untuk meningkatkan keterampilan tim saya dan mengikuti perkembangan teknologi terbaru. Aplikasi online class ini benar-benar membantu kami untuk mencapai tujuan tersebut. Dengan akses ke berbagai kursus berkualitas tinggi, kami dapat belajar kapan saja dan di mana saja. Materi yang disediakan sangat relevan dan mudah dipahami, terutama dalam bidang desain dan teknologi.",
+    },
+    {
+      "photo": "https://picsum.photos/400",
+      "name": "Gloria Arnasari",
+      "jobTitle": "Head of Designer at PT. XYZ",
+      "title": "Pengalaman belajar mendorong saya ke level berikutnya",
+      "review":
+          "Saya selalu mencari cara untuk meningkatkan keterampilan tim saya dan mengikuti perkembangan teknologi terbaru. Aplikasi online class ini benar-benar membantu kami untuk mencapai tujuan tersebut. Dengan akses ke berbagai kursus berkualitas tinggi, kami dapat belajar kapan saja dan di mana saja. Materi yang disediakan sangat relevan dan mudah dipahami, terutama dalam bidang desain dan teknologi.",
+    },
+  ];
 }
--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -1,22 +1,238 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:pijarmahir/app/data/fonts.dart';
 
 import '../controllers/dashboard_controller.dart';
 
 class DashboardView extends GetView<DashboardController> {
-  const DashboardView({Key? key}) : super(key: key);
+  const DashboardView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DashboardView'),
-        centerTitle: true,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text('Pijar')),
+            ListTile(title: const Text('Item 1'), onTap: () {}),
+            ListTile(title: const Text('Item 2'), onTap: () {}),
+          ],
+        ),
       ),
-      body: const Center(
-        child: Text(
-          'DashboardView is working',
-          style: TextStyle(fontSize: 20),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 3,
+        shadowColor: Colors.black,
+        title: Text('Dashboard', style: mediumText24),
+        centerTitle: false,
+        actions: [
+          Image.asset("assets/icons/ic_ranks.png", width: 30),
+          const SizedBox(width: 8),
+          Image.asset("assets/icons/ic_shopping_cart.png", width: 30),
+          const SizedBox(width: 8),
+          Image.asset("assets/icons/ic_notifications.png", width: 30),
+          const SizedBox(width: 8),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // HERO Banner
+            Container(
+              child: Image.asset("assets/images/img_pijar_ads.png"),
+            ),
+
+            // About
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Text(
+                      "Tentang Pijar",
+                      style: mediumText24.copyWith(fontSize: 27),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: Text(
+                      "Platform pembelajaran dan sertifikasi secara digital yang memberikan kemudahan akses bagi masyarakat dalam meningkatkan keahlian dan kompetensinya serta sesuai dengan kebutuhan Industri pada saat ini maupun mendatang. Masyarakat dapat melakukan pembelajaran dengan mudah baik secara online maupun offline melalui platform ini.",
+                      style: regularText14,
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            // Category Grid
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 20.0,
+                  childAspectRatio: 0.7,
+                ),
+                itemCount: controller.categories.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(controller.categories[index]["image"]!,
+                          width: 80),
+                      const SizedBox(height: 8),
+                      Text(
+                        controller.categories[index]["name"]!,
+                        style: mediumText16,
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  );
+                },
+              ),
+            ),
+
+            // Ads Banner
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Image.asset("assets/images/img_ads_banner.png"),
+            ),
+
+            // Partners
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 30, 16, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Mitra Kerjasama",
+                    style: mediumText24,
+                  ),
+                  GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      crossAxisSpacing: 16.0,
+                      mainAxisSpacing: 0.8,
+                      childAspectRatio: 1,
+                    ),
+                    itemCount: controller.partners.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.asset(controller.partners[index], width: 80),
+                        ],
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+            // Review
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 50),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Dengarkan kisah inspiratif mereka",
+                    style: mediumText20,
+                  ),
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    height: 300,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: controller.reviews.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Card(
+                          margin: const EdgeInsets.only(
+                              left: 8.0, right: 8.0, bottom: 20),
+                          color: Colors.white,
+                          elevation: 5,
+                          child: Container(
+                            width: 280,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 20),
+                            child: Column(
+                              children: [
+                                // Profile
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 30.0,
+                                      backgroundColor: Colors.transparent,
+                                      backgroundImage: NetworkImage(
+                                          controller.reviews[index]['photo']!),
+                                    ),
+                                    const SizedBox(width: 10),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            controller.reviews[index]['name']!,
+                                            style: regularText18,
+                                          ),
+                                          Text(
+                                            controller.reviews[index]
+                                                ['jobTitle']!,
+                                            style: regularText16.copyWith(
+                                                color: Colors.black54),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 12),
+
+                                // Review Text
+                                Column(
+                                  children: [
+                                    Text(
+                                      controller.reviews[index]['title']!,
+                                      style: mediumText18,
+                                    ),
+                                    const SizedBox(height: 4),
+                                    Text(
+                                      controller.reviews[index]['review']!,
+                                      style: regularText14.copyWith(
+                                          color: Colors.black87),
+                                      maxLines: 3,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
